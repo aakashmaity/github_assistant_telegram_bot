ea78ac15c062cc0d8015aa3c4d05e42aa082dffa
aakashmaity <aakashmaity57@gmail.com>
2025-08-30 21:52:46 +0530

    Added startup command popup

--- src/bot/commands/start.js
+++ src/bot/commands/start.js
@@ -1,5 +1,13 @@
 export default (bot) => {
   bot.start((ctx) => {
-    ctx.reply(`👋 Welcome, ${ctx.from.first_name}!\n Use /help to see what I can do.`);
+    ctx.reply(`👋 Welcome, ${ctx.from.first_name}!\n Use /help to see what I can do.`,
+      {
+        reply_markup: {
+          keyboard: [['/openprs', '/review']],
+          resize_keyboard: true,
+          one_time_keyboard: true,
+        },
+      }
+    );
   });
 };
